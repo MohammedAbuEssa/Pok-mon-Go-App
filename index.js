7522d489d83946ae789a2c42ee49bb04e66be24f
MohammedAbuEssa <mabuissa5@gmail.com>
2023-11-01 20:28:05 +0000

    Done with the documentation

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ require("dotenv").config();
 
 const { start } = require("./src/server");
 const { sequelize } = require("./src/model/index");
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8080;
 const db = sequelize;
 
-db.sync({ force: false, alter: true }).then(() => {
+db.sync({ force: true, alter: true }).then(() => {
   start(port);
 });
